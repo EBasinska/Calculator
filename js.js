6b020ad0a0a58cfb72c0d3c540f3bc1750b00b7e
EBasinska <84963573+EBasinska@users.noreply.github.com>
2021-06-07 18:58:56 +0100

    css for calc
    css for calc

---
+++ js.js
@@ -0,0 +1,26 @@
+let answer = 0;
+let currentDisplay = 0;
+let addTo = 0;
+let multiplyBy = 0;
+let divideBy = 0;
+let SubtractFrom = 0;
+
+function addition(answer, addTo) {
+    answer = answer + addTo;
+  }
+
+function subtraction(answer, subtractFrom) {
+    answer = answer - subtractFrom;
+  }
+
+function division(answer, divideBy) {
+    answer = answer / divideBy;
+  }
+
+function multiplication(answer, multiplyBy) {
+    answer = answer * multiplyBy;
+  }
+
+function multiplication(answer, multiplyBy) {
+    answer = answer / divideBy;
+  }
