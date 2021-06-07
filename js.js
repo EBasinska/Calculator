1f9b1e1a018d436a760776732a266ec893ad9b79
Ksymena Basińska <ksymena.basinska@miquido.com>
2021-06-07 20:09:27 +0200

    Formatted all files. Removed a redundant function

--- js.js
+++ js.js
@@ -6,21 +6,17 @@ let divideBy = 0;
 let SubtractFrom = 0;
 
 function addition(answer, addTo) {
-    answer = answer + addTo;
-  }
+  answer = answer + addTo;
+}
 
 function subtraction(answer, subtractFrom) {
-    answer = answer - subtractFrom;
-  }
+  answer = answer - subtractFrom;
+}
 
 function division(answer, divideBy) {
-    answer = answer / divideBy;
-  }
+  answer = answer / divideBy;
+}
 
 function multiplication(answer, multiplyBy) {
-    answer = answer * multiplyBy;
-  }
-
-function multiplication(answer, multiplyBy) {
-    answer = answer / divideBy;
-  }
+  answer = answer * multiplyBy;
+}
